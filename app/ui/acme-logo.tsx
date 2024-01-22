58058cccd7250deb5748f92836b16e3889fb5bdf
RichardOsei <59200416+RichardOsei@users.noreply.github.com>
2024-01-22 16:45:02 +0000

    updated

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,4 +1,4 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { ShoppingCartIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
@@ -6,8 +6,8 @@ export default function AcmeLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <ShoppingCartIcon className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-2xl">Peer Store</p>
     </div>
   );
 }
