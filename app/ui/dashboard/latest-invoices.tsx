8669268feb52116db7e3abaeeecb34237c6176cf
RichardOsei <59200416+RichardOsei@users.noreply.github.com>
2024-01-23 17:05:52 +0000

    updated

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import { LatestInvoice } from '@/app/lib/definitions';
+import { UserCircleIcon } from '@heroicons/react/20/solid';
 export default async function LatestInvoices({
   latestInvoices,
 }: {
@@ -29,13 +30,7 @@ export default async function LatestInvoices({
                 )}
               >
                 <div className="flex items-center">
-                  <Image
-                    src={invoice.image_url}
-                    alt={`${invoice.name}'s profile picture`}
-                    className="mr-4 rounded-full"
-                    width={32}
-                    height={32}
-                  />
+                  <UserCircleIcon/>
                   <div className="min-w-0">
                     <p className="truncate text-sm font-semibold md:text-base">
                       {invoice.name}
