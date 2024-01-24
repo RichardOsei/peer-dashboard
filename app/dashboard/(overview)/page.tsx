65d5dec40a21aed52eeec1bc128efbde973a7337
RichardOsei <59200416+RichardOsei@users.noreply.github.com>
2024-01-24 17:44:23 +0000

    added skeleton to the dashboard

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -21,8 +21,8 @@ export default async function Page() {
         <Card title="Total Inventories" value={totolInventories}type="inventories"/> 
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-         <RevenueChart revenue={revenue}  />
-         <LatestInvoices latestInvoices={latestInvoices} />
+        <RevenueChart revenue={revenue}  />
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
