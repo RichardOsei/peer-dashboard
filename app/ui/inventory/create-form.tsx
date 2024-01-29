e78d8a8d2d0385e6754a0494648b8dc76e70345f
RichardOsei <59200416+RichardOsei@users.noreply.github.com>
2024-01-29 22:28:33 +0000

    UPDATED

--- app/ui/inventory/create-form.tsx
+++ app/ui/inventory/create-form.tsx
@@ -1,6 +1,6 @@
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
-import {  CheckIcon,  ClockIcon,  CurrencyDollarIcon,  UserCircleIcon,} from '@heroicons/react/24/outline';
+import {  CheckIcon,  ClockIcon,  CurrencyDollarIcon,  PencilSquareIcon,} from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 import { createInvoice } from '@/app/lib/actions';
 
@@ -11,32 +11,32 @@ export default function InventoryForm({ customers }: { customers: CustomerField[
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
-            Choose customer
+            Activity
           </label>
           <div className="relative">
-            <select
-              id="customer"
-              name="customerId"
+            <input
+              id="Activity"
+              name="Add Activity"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
             >
-              <option value="" disabled>
-                Select a customer
+              {/*<option value="" disabled>
+                Activity
               </option>
               {customers.map((customer) => (
                 <option key={customer.id} value={customer.id}>
                   {customer.name}
-                </option>
-              ))}
-            </select>
-            <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+              </option>
+              ))}*/}
+            </input>
+            <PencilSquareIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
         </div>
 
         {/* Invoice Amount */}
         <div className="mb-4">
           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
-            Choose an amount
+            Amount
           </label>
           <div className="relative mt-2 rounded-md">
             <div className="relative">
@@ -45,7 +45,7 @@ export default function InventoryForm({ customers }: { customers: CustomerField[
                 name="amount"
                 type="number"
                 step="0.01"
-                placeholder="Enter USD amount"
+                placeholder="Enter amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
@@ -56,7 +56,7 @@ export default function InventoryForm({ customers }: { customers: CustomerField[
         {/* Invoice Status */}
         <fieldset>
           <legend className="mb-2 block text-sm font-medium">
-            Set the invoice status
+            Set status
           </legend>
           <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
             <div className="flex gap-4">
