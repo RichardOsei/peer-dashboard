8eafe698d61d07c51eda54a1fc637fcf52696cc3
RichardOsei <59200416+RichardOsei@users.noreply.github.com>
2024-01-30 22:53:59 +0000

    updated to extract data from formdata

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,3 +1,21 @@
 'use server';
  
-export async function createInvoice(formData: FormData) {}
+export async function createInvoice(formData: FormData) {
+  const rawFormData = {
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  };
+  // Test it out:
+  console.log(rawFormData);
+}
+
+export async function createInventory(formData: FormData) {
+    const rawFormData = {
+      activity: formData.get('activity'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    };
+    // Test it out:
+    console.log(rawFormData);
+  }
