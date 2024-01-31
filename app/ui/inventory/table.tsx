2ad31d9812530fa062a79fa029992f9d2436f483
RichardOsei <59200416+RichardOsei@users.noreply.github.com>
2024-01-31 17:31:04 +0000

    updated

--- app/ui/inventory/table.tsx
+++ app/ui/inventory/table.tsx
@@ -53,10 +53,10 @@ export default async function InvoicesTable({
             <thead className="rounded-lg text-left text-sm font-normal">
               <tr>
                 <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
-                  Customer
+                  Activity
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
-                  Email
+                  Quantity
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
                   Amount
