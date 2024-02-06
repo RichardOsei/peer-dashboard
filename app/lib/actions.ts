2f7352f7cd9c89c3b92dcb1769ed98035c98e7e4
RichardOsei <59200416+RichardOsei@users.noreply.github.com>
2024-02-06 13:34:27 +0000

    updated with server side error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,26 +7,67 @@ import { redirect } from 'next/navigation';
  
 const FormSchema = z.object({
   id: z.string(),
-  activity: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  quantity: z.coerce.number(),
-  unitPrice: z.coerce.number(),
-  status: z.enum(['pending', 'paid', 'received']),
+  activity: z.string(
+    {invalid_type_error: 'Please input activity.',}
+  ),
+  customerId: z.string(
+    {invalid_type_error: 'Please select a customer.',}
+  ),
+  amount: z.coerce.number().gt(
+    0,{ message: 'Please enter an amount greater than $0.' }
+  ),
+  quantity: z.coerce.number().gt(
+    0,{ message: 'Please enter an amount greater than $0.' }
+  ),
+  unitPrice: z.coerce.number().gt(
+    0,{ message: 'Please enter an amount greater than $0.' }
+  ),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an status.',
+  }),
   date: z.string(),
 });
  
 
+export type invoiceState = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export type inventoryState = {
+  errors?: {
+    activiy?: string[];
+    quantity?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true,activity: true,quantity: true,unitPrice: true });
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+
+export async function createInvoice(prevState: invoiceState, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
     });
     
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Create Invoice.',
+      };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const date = new Date().toISOString().split('T')[0];
+
     try {
     await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
@@ -38,9 +79,6 @@ export async function createInvoice(formData: FormData) {
   }
 
 
-    // Test it out:
-    //console.log(formData);
-    //redirect to invoices page
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
   
@@ -48,7 +86,7 @@ export async function createInvoice(formData: FormData) {
 
 
 const CreateInventory = FormSchema.omit({ id: true, date: true,customerId: true,amount:true});
-export async function createInventory(formData: FormData) {    
+export async function createInventory(prevState: inventoryState,formData: FormData) {    
     const { activity,quantity,unitPrice, status } = CreateInventory.parse({
         activity: formData.get('activity'),
         unitPrice: formData.get('unitPrice'),
